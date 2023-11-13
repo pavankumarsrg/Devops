8bfba71357b8aaaffa965f3cbddddafef21c48e5
pavankumarsrg <129925606+pavankumarsrg@users.noreply.github.com>
2023-11-13 14:59:30 +0530

    Update Installing.sh

--- Docker/Installing.sh
+++ Docker/Installing.sh
@@ -10,3 +10,4 @@ sudo chmod a+r /etc/apt/keyrings/docker.gpg
 echo "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo snap install docker 20.10.24
